407b79b94603b41fc61ef2c87c83b6f47eb6f116
unknown <santi.volcanos@gmail.com>
2023-03-15 19:25:00 -0600

    Homeworks

---
+++ Homeworks_Santiago.cpp
@@ -0,0 +1,77 @@
+// Homeworks_Santiago.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int choice;
+int main()
+{
+    cout << "Bienvenido a mis tareas, escoga de la lista.\n 1.-\n2.-\n3.-\n4.-\n5.-\n6.-\n";
+    cin >> choice;
+	switch (choice)
+	{
+	case 1:
+        
+
+            srand(static_cast<unsigned int>(time(0)));
+            //srand(time(NULL));
+            int randomNumber = rand();
+            int secretNumber = (randomNumber % 100) + 1;
+            int tries = 0;
+            int guess;
+
+            cout << "GUESS MY NUMBER" << endl;
+            cout << "Adivina el número en el menor número de intentos posible." << endl;
+
+            //Depurar
+            cout << secretNumber;
+            do
+            {
+               
+                cout << "\nAdivina un numero! (1-100):" << endl;
+                cin >> guess;
+                tries++;
+                int VC = secretNumber - guess;
+                if (VC >= -5 && VC <= 5 && VC!=0)
+                {
+                    cout << "Estas muy cerca!\n";
+                }
+                if (guess > secretNumber) {
+                    cout << "Muy Alto\n\n";
+
+                }
+                else if (guess < secretNumber) {
+                    cout << "Muy Bajo\n\n";
+
+                }
+                else {
+                    cout << "Excelente lo hiciste en -- " << tries << "-- intentos";
+
+                }
+            } while (guess != secretNumber);
+        
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	default:
+		break;
+	}
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
